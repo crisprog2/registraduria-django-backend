f4525251959be9c375a9f01826542af2f40a3751
christian <christian_kazuma@hotmail.com>
2025-09-03 11:30:05 -0500

    Implementación de autenticación JWT, usuario personalizado y protección de endpoints

--- registraduria/urls.py
+++ registraduria/urls.py
@@ -11,11 +11,30 @@ Incluye rutas para:
 """
 
 
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+# Configuración para que Swagger acepte JWT (Bearer)
+from drf_yasg import openapi
+from drf_yasg import app_settings as yasg_settings
+
+yasg_settings.swagger_settings.SECURITY_DEFINITIONS = {
+    'Bearer': {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header',
+        'description': 'JWT Authorization header using the Bearer scheme. Ejemplo: "Bearer {token}"',
+        'x-example': 'Bearer <tu_token_jwt>'
+    }
+}
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -25,6 +44,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    authentication_classes=[],
 )
 
 urlpatterns = [
@@ -32,4 +52,6 @@ urlpatterns = [
     path('api/', include('core.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
